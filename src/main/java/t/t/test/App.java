ea1d625a6afb5c61ef63aed9811417c2801116ce
compaq <compaq@DESKTOP-3RRHVJJ>
2016-09-29 19:10:25 +0800

    second

--- src/main/java/t/t/test/App.java
+++ src/main/java/t/t/test/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! b1" );
+        System.out.println( "Hello World! b1 b1" );
     }
 }
